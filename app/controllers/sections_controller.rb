8efbe72899f0ca8722b488749a4fdd3a3a135dea
Joshua Rinard and Kevin Finley <kevin@>
2017-02-07 10:57:29 -0800

    add crud functionality for sections

---
+++ app/controllers/sections_controller.rb
@@ -0,0 +1,52 @@
+class SectionsController < ApplicationController
+
+  def show
+    @section = Section.find(params[:chapter_id]) # finds id of a single list
+  end
+
+  def new
+    @chapter = Chapter.find(params[:chapter_id])
+    @section = @chapter.sections.new
+  end
+
+  def create
+    @chapter = Chapter.find(params[:chapter_id])
+    @section = @chapter.sections.new(section_params)
+    if @section.save
+      flash[:notice] = "Section Saved!"
+      redirect_to chapter_path(@section.chapter)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @chapter = Chapter.find(params[:chapter_id])
+    @section = Section.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @chapter = Chapter.find(params[:chapter_id])
+    @section = Section.find(params[:id])
+    if @section.update(section_params)
+      flash[:notice] = "Section Updated!"
+      redirect_to chapter_path(@section.chapter)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @chapter = Chapter.find(params[:chapter_id])
+    @section = Section.find(params[:id])
+    @section.destroy
+    flash[:notice] = "Section Deleted"
+    redirect_to chapter_path(@section.chapter)
+  end
+
+  private
+    def section_params
+      params.require(:section).permit(:name)
+    end
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207180723) do
+ActiveRecord::Schema.define(version: 20170207182251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chapters", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "chapter_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
